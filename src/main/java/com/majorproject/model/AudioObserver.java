8a272b934edaf65750ded77bc2ea3d17c7aea6e8
Aidan <111407481+amusasrd@users.noreply.github.com>
2022-10-27 23:33:36 +1100

    Delete src directory

--- src/main/java/com/majorproject/model/AudioObserver.java
+++
@@ -1,6 +0,0 @@
-package com.majorproject.model;
-
-/** The interface for classes involved in playing the theme song. */
-public interface AudioObserver {
-    void update();
-}
--- src/main/java/com/majorproject/view/StartWindow.java
+++
@@ -1,7 +0,0 @@
-package com.majorproject.view;
-
-/** The interface for windows that are displayed before the main window appears on screen. */
-public interface StartWindow
-{
-    void displayWindow();
-}
